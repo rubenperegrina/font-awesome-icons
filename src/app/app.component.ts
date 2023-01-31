767c9c190cfb9ae6fc4e7a41632002fe3908dae0
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-01-31 13:11:09 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faCoffee, faHome, faUser, faCheck, faImage, faPhone, faCircleXmark, faComment, faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStar, faFaceAngry, faCalendar, faBookmark, faLemon, faFileExcel, faMoon, faCreditCard, faFolder, faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faApple, faTiktok, faGithub, faYoutube, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'font-awesome-icons';
+export class AppComponent implements OnInit {
+  regularIconList = new Array;
+  solidIconList = new Array;
+  brandIconList = new Array;
+
+  ngOnInit(): void {
+    this.regularIconList.push(faHeart, faStar, faFaceAngry, faCalendar, faBookmark, faLemon, faFileExcel, faMoon, faCreditCard, faFolder, faPaperPlane);
+    this.solidIconList.push(faCoffee, faHome, faUser, faCheck, faImage, faPhone, faCircleXmark, faComment, faBolt);
+    this.brandIconList.push(faFacebook, faInstagram, faTwitter, faApple, faTiktok, faGithub, faYoutube, faGoogle, faLinkedin);
+  }
 }
